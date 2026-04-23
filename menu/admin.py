1057b88b76477f40b782171ef5d7e5259963a4c4
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-23 15:21:52 -0300

    Admin: Registered Category, Product and User models

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'is_available')
+    list_filter = ('category', 'is_available')
+    search_fields = ('name', 'description')
+    list_editable = ('price', 'is_available') # Permite editar direto na lista
