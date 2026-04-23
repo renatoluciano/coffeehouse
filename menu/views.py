ca0badf17cd819e14748a7d7e241c85703e961f8
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-23 17:27:26 -0300

    Feature: Shopping cart system using sessions and bug fixes in templates

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from .cart import Cart # Importa a nossa nova classe
 
 def index(request):
     products = Product.objects.all()
@@ -8,3 +9,21 @@ def index(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'menu/product_detail.html', {'product': product})
+
+# --- NOVAS VIEWS DO CARRINHO ---
+
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product)
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'menu/cart_detail.html', {'cart': cart})
--- setup/urls.py
+++ setup/urls.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from menu import views as menu_views 
 from accounts import views as accounts_views
-from orders import views as orders_views # Importe orders
+from orders import views as orders_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', menu_views.index, name='index'),
     path('product/<int:pk>/', menu_views.product_detail, name='product_detail'),
+    
+    # Cart Routes
+    path('cart/', menu_views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', menu_views.cart_add, name='cart_add'),
+    path('cart/remove/<int:product_id>/', menu_views.cart_remove, name='cart_remove'),
     
     # Auth & Profile
     path('signup/', accounts_views.signup, name='signup'),
@@ -20,8 +25,6 @@ urlpatterns = [
     
     # Orders
     path('order/create/<int:product_id>/', orders_views.create_order, name='create_order'),
-
-    # Barista Routes
     path('barista/', orders_views.barista_dashboard, name='barista_dashboard'),
     path('barista/status/<int:order_id>/<str:new_status>/', orders_views.update_status, name='update_status'),
 ]
