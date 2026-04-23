70057cdddd6c43b4d9cb0e0de1f96e3c098d8609
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-23 14:35:45 -0300

    Refactor models to English names

--- menu/models.py
+++ menu/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Nome da Categoria")
+    name = models.CharField(max_length=100)
 
     class Meta:
-        verbose_name_plural = "Categorias"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    name = models.CharField(max_length=150, verbose_name="Nome do Café")
-    description = models.TextField(verbose_name="Descrição")
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Preço")
-    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Foto")
-    is_available = models.BooleanField(default=True, verbose_name="Disponível")
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
