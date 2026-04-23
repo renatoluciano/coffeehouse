13774d242cd2739635cadce07067d26e8f6b59ec
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-23 14:12:50 -0300

    Configurado Custom User no app accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Campo para saber se é funcionário da cafeteria
+    is_coffee_staff = models.BooleanField(default=False)
+    # Campo para telefone (útil para avisar que o café está pronto!)
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
--- setup/settings.py
+++ setup/settings.py
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'accounts.User'
